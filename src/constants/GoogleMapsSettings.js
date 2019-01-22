f0a8afcb8c91d9e92b321ee36fbfcb5e1cfc5bdd
Milos Matovic <milos.matovic@odt.net>
2019-01-22 10:19:05 +0100

    remove google API key from commits

--- src/constants/GoogleMapsSettings.js
+++ src/constants/GoogleMapsSettings.js
@@ -1,8 +1,8 @@
 import React from "react";
+import { GoogleApiKey } from './GoogleApiKey';
 
-export const apiKey = "AIzaSyAjAVMLSEX2dNOxewpBgxyC2zElzO4TDfY";
 export const googleMapsSettings = {
-  googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${apiKey}&v=3.exp&libraries=geometry,drawing,places`,
+  googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${GoogleApiKey}&v=3.exp&libraries=geometry,drawing,places`,
   loadingElement: <div style={{ height: `100%` }} />,
   containerElement: <div style={{ height: `400px` }} />,
   mapElement: <div style={{ height: `100%` }} />
